0af522998395e1cf9f0a50f86c00a0ca38a24ab4
Surxe <119145352+Surxe@users.noreply.github.com>
2025-04-16 19:26:25 -0400

    replace docker launch commands with docker-swarm launch commands

--- src/scripts/launch.sh
+++ src/scripts/launch.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker-compose down
+docker stack rm mystack # Remove stack if it exists
 
 docker build -t mywebapp:latest -f ./src/app/Dockerfile.app ./src/app # Build image
 
-docker-compose up --build --scale web=3 -d
+docker stack deploy -c stack.yml mystack # Start stack
--- src/scripts/setup.sh
+++ src/scripts/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo usermod -aG docker $USER # Add user to docker group so sudo prefix isn't needed
+
+docker swarm init
