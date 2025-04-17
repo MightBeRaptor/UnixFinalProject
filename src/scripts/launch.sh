74e475c573977a919949327e84e583f23d37ae43
Surxe <119145352+Surxe@users.noreply.github.com>
2025-04-16 21:42:55 -0400

    Add sleeps in launch.sh

--- src/scripts/launch.sh
+++ src/scripts/launch.sh
@@ -2,6 +2,10 @@
 
 docker stack rm mystack # Remove stack if it exists
 
+sleep 5
+
 docker build -t mywebapp:latest -f ./src/app/Dockerfile.app ./src/app # Build image
+
+sleep 5
 
 docker stack deploy -c stack.yml mystack # Start stack
