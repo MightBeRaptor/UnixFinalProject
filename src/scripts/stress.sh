78c25c9957fefca87eef18d9ab27605cf8c43d90
Surxe <119145352+Surxe@users.noreply.github.com>
2025-04-18 15:01:32 -0400

    stress.sh stresses containers instead of master

--- src/scripts/stress.sh
+++ src/scripts/stress.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
-stress-ng --cpu $(nproc) -t 5m
+# Get all container IDs for the web service
+containers=$(docker ps --filter "name=mystack_web" --format "{{.ID}}")
+
+for container in $containers; do
+    # Get CPU usage for each container
+    docker exec $container stress-ng --cpu $(nproc) -t 60s
